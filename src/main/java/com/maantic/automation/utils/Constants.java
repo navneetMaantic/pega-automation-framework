d8e360e867cc9714ec1e0ab945893e23716c2222
navneetMaantic <116359943+navneetMaantic@users.noreply.github.com>
2024-08-22 11:09:58 +0530

    final changes

--- src/main/java/com/maantic/automation/utils/Constants.java
+++ src/main/java/com/maantic/automation/utils/Constants.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class Constants {
 //    public static final String TEST_DATA_SHEET_PATH= System.getProperty("user.dir")+"\\src\\main\\resources\\exceltestdata\\GBT.xlsx";
     public static final String TEST_DATA_SHEET_PATH= "D:\\GBT\\GBT.xlsx";
-    public static final String TEST_OUT_DATA_SHEET_PATH = "D:\\GBT\\"+ "GBT_Results_"+outFileName()+".xlsx";
+    public static final String TEST_OUT_DATA_SHEET_PATH = "D:\\GBT\\GBT-Output\\"+ "GBT_Results_"+outFileName()+".xlsx";
 //    public static final String TEST_OUT_DATA_SHEET_PATH= System.getProperty("user.dir")+"\\src\\main\\resources\\output\\GBT_Results_"+outFileName()+".xlsx";
     public static final String EXCEL_SHEET_NAME = "GBT";
     public static final int IMPLICIT_WAIT_TIME= 30;
